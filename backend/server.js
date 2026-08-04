3aedd2125f2fae8d32b96567faa7d857221db560
Clarissa Hwang <clarissah99@gmail.com>
2026-08-04 19:42:40 -0400

    Implement POST /tasks endpoint

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,18 @@ app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
+app.post("/tasks", (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  res.status(201).json(newTask);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
